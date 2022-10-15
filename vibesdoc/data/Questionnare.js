16a3d51b1e0fc9d3c3e81a06fd0eeb15820ae059
sameehsuhail <sameehsuhail5@gmail.com>
2022-10-14 18:30:44 -0700

    dev 1

---
+++ vibesdoc/data/Questionnare.js
@@ -0,0 +1,22 @@
+const questions = [
+  {
+    question: "Question 1?",
+    answers: [
+      { id: "1", text: "1" },
+      { id: "2", text: "2" },
+      { id: "3", text: "3" },
+      { id: "4", text: "4" }
+    ]
+  },
+  {
+    question: "Question2",
+    answers: [
+      { id: "1", text: "6" },
+      { id: "2", text: "7" },
+      { id: "3", text: "8" },
+      { id: "4", text: "9" }
+    ]
+  }
+];
+
+export default questions;
--- vibesdoc/screens/Home.js
+++ vibesdoc/screens/Home.js
@@ -1,10 +1,55 @@
-import React from 'react'
-import { Text, View } from 'react-native';
 
-function Home() {
-  return (
-        <Text>Hello</Text>
-  )
-}
+import React from "react";
+import { ScrollView, StatusBar, StyleSheet } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+import Qpage from "../data/Questionnare";
+import Rpage from "../data/Results";
+import Tpage from "../data/tests";
 
-export default Home
+import { RowItem } from "../components/RowItem";
+
+export default ({ navigation }) => (
+  <ScrollView style={styles.container}>
+    <StatusBar barStyle="dark-content" />
+    <RowItem
+      name="Questionnare"
+      color="#fff"
+      onPress={() =>
+        navigation.navigate("Quiz", {
+          title: "Questionnare",
+          questions: Qpage,
+          color: "#36b1f0"
+        })
+      }
+    />
+    <RowItem
+      name="Results"
+      color="#fff"
+      onPress={() =>
+        navigation.navigate("Quiz", {
+          title: "Results",
+          questions: Rpage,
+          color: "#799496"
+        })
+      }
+    />
+    <RowItem
+      name="tests"
+      color="#fff"
+      onPress={() =>
+        navigation.navigate("Quiz", {
+          title: "tests",
+          questions: Tpage,
+          color: "#49475B"
+        })
+      }
+    />
+  </ScrollView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+  },
+
+});
